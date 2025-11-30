1787be608bd5d3015be9c542d9bb5e059d587181
Matteo Vaccari <vaccari@pobox.com>
2025-11-30 18:06:20 +0100

    Add terrain properties

--- src/domain/terrain/Terrain.ts
+++ src/domain/terrain/Terrain.ts
@@ -2,6 +2,11 @@
 export abstract class Terrain {
     public imagePath: string | undefined;
     public name: string = "";
+    public unitMovingInMustStop = false;
+    public unitMovingInCannotBattle = false;
+    public infantryBattleInReduction = 0;
+    public armorBattleInReduction = 0;
+    public armorBattleOutReduction = 0;
     toString(): string {
         return this.name;
     }
@@ -20,18 +25,31 @@ export const hillTerrain = new HillTerrain();
 
 class WoodsTerrain extends Terrain {
     name = "Woods";
+    unitMovingInMustStop = true;
+    unitMovingInCannotBattle = true;
+    infantryBattleInReduction = 1;
+    armorBattleInReduction = 2;
     readonly imagePath = "images/terrain/woods.png";
 }
 export const woodsTerrain = new WoodsTerrain();
 
 class HedgerowsTerrain extends Terrain {
     name = "Hedgerows";
+    unitMovingInMustStop = true;
+    unitMovingInCannotBattle = true;
+    infantryBattleInReduction = 1;
+    armorBattleInReduction = 2;
     readonly imagePath = "images/terrain/hedgerows.png";
 }
 export const hedgerowsTerrain = new HedgerowsTerrain();
 
 export class TownTerrain extends Terrain {
     name = "Town";
+    unitMovingInMustStop = true;
+    unitMovingInCannotBattle = true;
+    infantryBattleInReduction = 1;
+    armorBattleInReduction = 2;
+    armorBattleOutReduction = 2;
     constructor(imagePath: string) {
         super();
         this.imagePath = "images/terrain/" + imagePath;
